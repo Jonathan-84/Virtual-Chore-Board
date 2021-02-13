fecd7c9d11f178979dbc00e5308772151775e54c
JRob <72467031+Jonathan-84@users.noreply.github.com>
2021-02-12 21:20:53 -0500

    added role field to the user table

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,16 +1,22 @@
 INSERT INTO tasks (task_name, task_points) 
 VALUES ('Take out Trash',10),
-('Walk Dog',50);
+('Walk Dog',50),
+('Clean Bedroom',5),
+('Do the dishes',10),
+('Take out Trash',10),
+('Clean the Attic',30);
 
 INSERT INTO  child (child_name, current_points, banked_points)
- VALUES ('Timmy',10000, 0),
- ('Jennifer',2000, 0)
+ VALUES ('Fox',10000, 0),
+ ('Dana',2000, 0),
+ ('Jimmy',2000, 0)
 ;
 
 
-INSERT INTO user (name, email, password)
+INSERT INTO user (name, email, password, role)
 VALUES
-('Walter', 'walter.skinner@hotmail.com', '1234password')
+('Walter', 'walter.skinner@hotmail.com', '1234password', 'Parent'),
+('Maria', 'Maria@hotmail.com', 'syndicate123', 'Babysitter')
 ;
 
   
